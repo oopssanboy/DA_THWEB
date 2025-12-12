94c8062090ca681ad4f61ec91a6460e0da3ac8c8
Shinne <115135806+oopssanboy@users.noreply.github.com>
2025-12-12 18:58:40 +0700

    Update getclient_google.php

--- views/client/auth/getclient_google.php
+++ views/client/auth/getclient_google.php
@@ -1,15 +1,6 @@
 <?php 
 function clientGoogle(){
-        $client_id ='929628426936-mcu12r63sn02t0l3j84t0vuofcle2lle.apps.googleusercontent.com'; 
-        $client_secret ='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'; 
-        $redirect_uri = 'http://localhost:3000/controler/google_controler.php'; 
-        $client = new Google_Client();
-        $client->setClientId($client_id);
-        $client->setClientSecret($client_secret);
-        $client->setRedirectUri($redirect_uri);
-        $client->addScope("email");
-        $client->addScope("profile");
-        return $client;
+        
    
 }
 
