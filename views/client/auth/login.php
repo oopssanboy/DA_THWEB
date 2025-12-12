ca4979cb34d57e4aca1a8c735c47b21a2b60a9f8
Shinne <115135806+oopssanboy@users.noreply.github.com>
2025-12-12 18:58:26 +0700

    Update login.php

--- views/client/auth/login.php
+++ views/client/auth/login.php
@@ -2,19 +2,7 @@
 if (!isset($_SESSION)) session_start();
 require_once __DIR__ . '/../../../autoload.php';
 if(isset($_GET['action']) && $_GET['action'] == 'login_facebook'){
-$fb = new Facebook\Facebook([
-  'app_id' => '1290514812929698', 
-  'app_secret' => 'de94d39c2646632fb85d7d224ecede58',
-  'default_graph_version' => 'v19.0',
-  ]);
 
-$helper = $fb->getRedirectLoginHelper();
-
-$permissions = ['email','public_profile']; 
-$loginUrl = $helper->getLoginUrl('http://localhost:3000/views/client/auth/fb-callback.php', $permissions);
-echo '<a href="' . htmlspecialchars($loginUrl) . '">Log in with Facebook!</a>';
-header('location:'. $loginUrl . '');
-exit;
 }
 if(isset($_GET['action']) && $_GET['action'] == 'login_google'){
     $client = clientGoogle();
